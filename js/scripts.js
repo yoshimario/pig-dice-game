2efc3d65ea9351f0e60be1006abc88cb8d5814dc
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-08-26 14:50:12 -0700

    Created an instance so that we can console.log it and check to see the output
    Co-authored-by: Sean Wilson <paintballsean@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,31 @@
 // Global Variables
-const playerOneScore, playerTwoScore;
+
 // Business Logic
 
-//This function holds what the player rolled and the current player
-
-function PlayerScore (rolledDice,currentScore) {
-  this.rolledDice = 0;
-  this.currentScore = 0;
+function PigDice () {
+  this.rolledDice = 0,
+  this.playerScore = 0
 }
 
-PlayerScore.prototype.rolldice = function () {
-  let randomNumber = Math.floor(Math.random()* 6) + 1);
+PigDice.prototype.rollDice = function() {
+  let randomNumber = Math.floor((Math.random()* 6) + 1);
     this.rolledDice = randomNumber;
-    console.log(randomNumber);
     
     if (randomNumber === 1) {
-      this.currentScore = 0;
+      this.playerScore = 0;
       this.rolledDice = 1;
       console.log("You are doomed for eternity as you have a 1! HAHA Try again!");
     } else if (randomNumber > 1) {
-      this.currentScore = randomNumber + this.currentScore
-      console.log(currentScore);
+      this.playerScore = this.playerScore + randomNumber
+      console.log(this.rolledDice);
     } else {
-    console.log("We ran into a problem!")
+    console.log("We ran into a problem!");
     } 
-
-});
+    return this.playerScore;
+}
 
 // User Interface
 
+let diceOutput = new PigDice();
+console.log(diceOutput.rollDice());
+console.log(diceOutput.rollDice());
